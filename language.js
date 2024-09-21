7e9478d992e4b2650368c76b6c93038bddbba5c9
Shakhzodbee <Bakhodirovichshakhzodbek@gmail.com>
2024-09-21 21:51:23 +0500

    delete file

--- language.js
+++ language.js
@@ -512,21 +512,33 @@ window.onload = updateUI;
 
 
 
-// const link = document.getElementById('myLinkerror');
-// const modal = document.getElementById('myModals');
-// const closeBtn = document.getElementsByClassName('closes')[0];
+    // Get the modal
+    var modal = document.getElementById("myModal")
 
-// link.addEventListener('click', function(event) {
-//     event.preventDefault(); // Havola standart harakatini to'xtatish
-//     modal.style.display = 'block'; // Modalni ko'rsatish
-// });
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtns")
 
-// closeBtn.addEventListener('click', function() {
-//     modal.style.display = 'none'; // Modalni yashirish
-// });
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0]
 
-// window.addEventListener('click', function(event) {
-//     if (event.target === modal) {
-//         modal.style.display = 'none'; // Modalni tashqaridan bosilganda yashirish
-//     }
-// });
+    // When the user clicks the button, open the modal 
+    btn.onclick = function () {
+      modal.style.display = "block"
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none"
+    }
+    var modal = document.getElementById("myModal")
+
+    function closeModal() {
+      modal.style.display = "none"
+    }
+
+    // Close modal when clicking outside of it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        closeModal()
+      }
+    }

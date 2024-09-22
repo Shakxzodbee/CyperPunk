b759b106659d2f8dbb1ef2c66cd7880b0cb0b2e2
Shakhzodbee <Bakhodirovichshakhzodbek@gmail.com>
2024-09-22 21:39:46 +0500

    update

--- modal.js
+++ modal.js
@@ -1,9 +1,15 @@
-  let modal = document.getElementById("myModal")
-  let btn = document.getElementById("myBtns")
-  let span = document.getElementsByClassName("close")[0]
-  let create = document.getElementById('createAccount')
-  let recovery = document.getElementById('recovery')
-  let headerContent = document.getElementsByClassName('header__logo')
+let headerContent = document.getElementsByClassName('header__logo')
+let recoveryButton = document.getElementById('recovery');
+let headerLogo = document.getElementById('headerLogo');
+let create = document.getElementById('createAccount')
+let span = document.getElementsByClassName("close")[0]
+let recovery = document.getElementById('recovery')
+let spans = document.getElementById('closebtns')
+let spanss = document.getElementById('closebtn')
+let modal = document.getElementById("myModal")
+let btn = document.getElementById("myBtns")
+
+
   btn.onclick = function () {
       modal.style.display = "block"
   }
@@ -23,8 +29,16 @@
     modal.style.display = "none"
   }
 
-  window.onclick = function (event) {
-      if (event.target == modal) {
-        closeModal()
-      }
-  }
+
+  create.addEventListener('click', ()=>{
+    headerLogo.style.display = 'none';
+  })
+  spanss.addEventListener('click', ()=>{
+    headerLogo.style.display = 'block'
+  })
+  recoveryButton.addEventListener('click', () => {
+    headerLogo.style.display = 'none';
+  });
+  spans.addEventListener('click', ()=>{
+    headerLogo.style.display = 'block'
+  })

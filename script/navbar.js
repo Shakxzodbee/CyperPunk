d5e1c3cbace0902fa38f0eff791d444690e79508
Shakhzodbee <Bakhodirovichshakhzodbek@gmail.com>
2024-09-26 23:27:23 +0500

    'update'

--- script/navbar.js
+++ script/navbar.js
@@ -1,5 +1,20 @@
 let bars = document.querySelector('#bars')
+let sub = document.querySelector('.sub')
+let down = document.querySelector('.down')
+bars.onclick = function () {
+    sub.style.display = "block"
+    down.classList.remove('.fa-chevron-circle-down')
+    down.classList.add('.fa-times')
+} 
 
-bars.addEventListener('click', ()=>{
-    
-})
+
+document.getElementById('igri').addEventListener('click', function () {
+    const list = document.getElementById('myList');
+    list.classList.toggle('hidden');
+});
+
+
+document.getElementById('font').addEventListener('click', function () {
+    const list = document.getElementById('myUl');
+    list.classList.toggle('hidden');
+});

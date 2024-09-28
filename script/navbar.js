d7772168770b6a69fd6b3e0e4f1aa3468ce744ff
Shakhzodbee <Bakhodirovichshakhzodbek@gmail.com>
2024-09-28 20:40:44 +0500

    work started

--- script/navbar.js
+++ script/navbar.js
@@ -1,12 +1,36 @@
 let bars = document.querySelector('#bars')
 let sub = document.querySelector('.sub')
 let down = document.querySelector('.down')
+let header = document.querySelector('header')
+let exitTimes = document.querySelector('#faTimes')
+let downInfo = document.querySelector('.down-info')
+let downTimes = document.querySelector('#downTimes')
 bars.onclick = function () {
     sub.style.display = "block"
-    down.classList.remove('.fa-chevron-circle-down')
-    down.classList.add('.fa-times')
-} 
+    header.style.display = 'none'
+    bars.style.display = 'none'
+    down.style.display = 'none'
+}
+exitTimes.addEventListener('click', ()=>{
+    header.style.display = 'block'
+    bars.style.display = 'block'
+    down.style.display = 'block'
+    sub.style.display = "none"
+})
 
+down.addEventListener('click',()=>{
+    downInfo.style.display = 'block'
+    header.style.display = 'none'
+    bars.style.display = 'none'
+    down.style.display = 'none'
+
+})
+downTimes.addEventListener('click' , ()=>{
+    downInfo.style.display = 'none'
+    header.style.display = 'block'
+    bars.style.display = 'block'
+    down.style.display = 'block'
+})
 
 document.getElementById('igri').addEventListener('click', function () {
     const list = document.getElementById('myList');
